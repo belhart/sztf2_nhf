77cf56b5b5cdc2c6d3f1d1e05db320b71827a989
belhart <43545373+belhart@users.noreply.github.com>
2019-04-20 15:00:40 +0200

    Exceptionok alapjai

---
+++ Sztf2_nhf/class/AlapException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sztf2_nhf
+{
+    class AlapException : Exception
+    {
+        public string HibaUzenet { get; set; }
+    }
+}
